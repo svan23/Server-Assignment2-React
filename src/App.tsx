b36d0e09db23ec7e641a1bbd7a7e3021e4a94a96
svan23 <vansla>
2025-03-19 17:30:18 -0700

    setup REACT

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css";
+import { Outlet } from "react-router";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+export default App;
